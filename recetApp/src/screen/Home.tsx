a3e3124159bbbaf59775d9abd29f260a81e9184b
Samuel Galo <samuelgalo1625@gmail.com>
2024-05-17 00:07:44 -0600

    Commit1
    Creacion de navegacion con react-navigation

--- recetApp/src/screen/Home.tsx
+++ recetApp/src/screen/Home.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={style.Page}>
+      <Image style={style.image} source={require('./images/icono-etiqueta-recetas-calabacin-cru.png')} />
+      <Text style={style.text}>La aplicacion permite a los usuarios
+      {'\n'}agregar nuevas recetas, ver la lista de
+      {'\n'}recetas disponibles, ver los detalles
+      {'\n'} de una receta especifica, y eliminar
+      {'\n'} recetas existentes
+      </Text>
+      <Text style={style.text}>
+        Dueño de la aplicacion:
+        {'\n'}Samuel Galo
+      </Text>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+    image: {
+      width: 100,
+      height: 150, 
+      alignSelf:'center',
+       marginTop: 20
+    },
+    Page: {
+      flex: 1
+    },
+    text: {
+      textAlign: 'center',
+      fontSize: 20,
+      marginTop: 10
+    }
+})
 
 export default Home
---
+++ recetApp/src/screen/ListReceta.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListReceta = () => {
+  return (
+    <View>
+      <Text>ListReceta</Text>
+    </View>
+  )
+}
+
+export default ListReceta
