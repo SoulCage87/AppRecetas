0099a8eed20cb0e27d44ece038995b43e31d60f7
Samuel Galo <samuelgalo1625@gmail.com>
2024-05-16 23:19:15 -0600

    Commit
    Instalando dependencias y librerias

---
+++ recetApp/src/screen/AddReceta.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddReceta = () => {
+  return (
+    <View>
+      <Text>AddReceta</Text>
+    </View>
+  )
+}
+
+export default AddReceta
